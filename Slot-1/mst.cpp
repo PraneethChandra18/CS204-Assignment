e6ef8db2ce74c785d699f9d4c506c423bd37d134
PraneethChandra18 <52711274+PraneethChandra18@users.noreply.github.com>
2019-11-14 13:52:32 +0530

    Add files via upload

---
+++ Slot-1/mst.cpp
@@ -0,0 +1,145 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Edge
+{
+public:
+	 int src,dest,weight;
+
+};
+class Graph
+{
+public:
+	int V,E;
+	Edge* edge;
+};
+Graph* createGraph(int V,int E)
+{
+	Graph* graph=new Graph;
+	graph->V=V;
+	graph->E=E;
+	graph->edge=new Edge[E];
+	return graph;
+}
+class subset
+{
+public:
+	int parent;
+	int rank;
+};
+int find(subset subsets[],int i)
+{
+	if(subsets[i].parent!=i)
+	{
+		subsets[i].parent=find(subsets,subsets[i].parent);
+	}
+	return subsets[i].parent;
+}
+void Union(subset subsets[],int x,int y)
+{
+	int xroot=find(subsets,x);
+	int yroot=find(subsets,y);
+
+	if(subsets[xroot].rank<subsets[yroot].rank)
+	{
+		subsets[xroot].parent=yroot;
+		return;
+	}
+	else if(subsets[xroot].rank>subsets[yroot].rank)
+	{
+		subsets[yroot].parent=xroot;
+		return;
+	}
+	else if(subsets[xroot].rank==subsets[yroot].rank)
+	{
+		subsets[yroot].parent=xroot;
+		subsets[xroot].rank++;
+		return;
+	}
+
+
+}
+int myComp(const void* a,const void* b)
+{
+	Edge* a1=(Edge*)a;
+	Edge* b1=(Edge*)b;
+	return a1->weight>b1->weight;
+}
+void KrushkalMSt(Graph* graph,Edge* k)
+{
+	int V=graph->V;
+	Edge result[V];
+	qsort(graph->edge,graph->E,sizeof(graph->edge[0]),myComp);
+	subset* subsets=new subset[(V*sizeof(subset))];
+	for(int v=0;v<V;++v)
+	{
+		subsets[v].parent=v;
+		subsets[v].rank=0;
+	}
+	int e=0;
+	int i=0;
+	while(e<V-1&&i<graph->E)
+	{
+		Edge next_edge=graph->edge[i++];
+		int x=find(subsets,next_edge.src);
+		int y=find(subsets,next_edge.dest);
+		if(x!=y)
+		{
+			result[e++]=next_edge;
+			Union(subsets,x,y);
+		}
+	}
+	for (int i = 0; i < e; ++i)
+		k[i]=result[i];
+
+	return ;
+
+
+}
+
+int main()
+{
+	/* code */
+	int V ; // Number of vertices in graph
+    int E ; // Number of edges in graph
+    cin>>V;
+    cin>>E;
+    Edge k[V];
+    Graph* graph = createGraph(V, E);
+
+    int x,y,w;
+    for(int i=0;i<E;i++)
+    {
+    	cin>>x;
+    	cin>>y;
+    	cin>>w;
+    	// add edge 0-1
+        graph->edge[i].src = x;
+        graph->edge[i].dest = y;
+        graph->edge[i].weight = w;
+    }
+
+
+    int sum=0;
+   KrushkalMSt(graph,k);
+   			for (int i = 0; i < V-1; ++i)
+   			{
+   			   sum=sum+graph->edge[i].weight;
+   			}
+
+		       cout<<sum;
+
+
+	return 0;
+
+}
+/*
+input:
+4 5
+1 2 7
+1 4 6
+4 2 9
+4 3 8
+2 3 6
+output:
+19
+*/
---
+++ Slot-1/shortestpath.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define INF 10000000
+
+int main()
+{    int V;
+    cin>>V;
+    int E;
+    cin>>E;
+     int x;
+        int y;
+      int w;
+    int graph[V][V];
+    for(int i=0;i<V; i++)
+	    {
+			for(int j=0;j<V;j++)
+			{
+				graph[i][j]=INF;
+			}
+		}
+
+
+    for(int i=0;i<V;i++)
+    {
+        graph[i][i]=0;
+    }
+    for(int i=0;i<E;i++)
+    {
+        cin>>x;
+        cin>>y;
+        cin>>w;
+        graph[x][y]=w;
+
+    }
+    int dist[V][V];
+	for(int i=0;i<V;i++)
+	{
+		for(int j=0;j<V;j++)
+		{
+			dist[i][j]=graph[i][j];
+		}
+	}
+	for(int k=0;k<V;k++)
+	{
+		for(int i=0;i<V; i++)
+	    {
+			for(int j=0;j<V;j++)
+			{
+				if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+
+			}
+		}
+	}
+
+	int sum=0;
+	int i,j;
+	    for(i=0;i<V;i++)
+	{
+		for( j=0;j<V;j++)
+		{
+			if(dist[i][j]== INF)
+			{
+			    break;
+			}
+			else
+			{
+			    sum=sum+dist[i][j];
+			}
+		}
+		if(j==V)
+		{
+		    cout<<"YES"<<" value of sum is "<<sum<<" city is "<<i<<endl;
+		    break;
+		}
+
+	}
+    if(i==V)
+    {
+        cout<<"NO";
+    }
+    return 0;
+}
+
+/*
+input :
+4
+4
+0 3 10
+0  1 5
+1 2 3
+2 3 1
+output:
+YES value of sum is 22 city is 0
+input:
+4
+4
+0 3 10
+0  1 5
+2  1 3
+2 3 1
+output:
+NO
+*/
